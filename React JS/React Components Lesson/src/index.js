cc35b90c1190dcd23ba4f776813aa6a027a0c7de
Gareque <grigeral@gmail.com>
2025-09-04 20:26:16 +0100

    Adding components lesson

---
+++ React JS/React Components Lesson/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./Header";
+import List from "./List";
+
+ReactDOM.render(
+  <div>
+    <Header />
+    <List />
+  </div>,
+  document.getElementById("root")
+);
