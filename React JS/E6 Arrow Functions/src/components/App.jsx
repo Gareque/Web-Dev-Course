e9c324c4098a7ccd573782026bf7516356d0252e
Gareque <grigeral@gmail.com>
2025-09-23 18:40:09 +0100

    Adding the lesson on arrow functions

---
+++ React JS/E6 Arrow Functions/src/components/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">
+        {/* This shows how to add an arrow function into the javascript in-code */}
+        {emojipedia.map((emojiTerm) => (
+          <Entry
+            key={emojiTerm.id}
+            emoji={emojiTerm.emoji}
+            name={emojiTerm.name}
+            description={emojiTerm.meaning}
+          />
+        ))}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
