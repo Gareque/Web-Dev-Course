f73a973760a861fb9bc301ec7eb0c6d20e3865f2
Gareque <grigeral@gmail.com>
2025-09-23 18:46:39 +0100

    Amending const to var to remove errors

--- React JS/E6 Arrow Functions/src/index.js
+++ React JS/E6 Arrow Functions/src/index.js
@@ -8,27 +8,27 @@ ReactDOM.render(<App />, document.getElementById("root"));
 var numbers = [3, 56, 2, 48, 5];
 
 //Traditional way of doing functions:
-const newNumber = numbers.map(function (x) {
+var newNumber = numbers.map(function (x) {
   return x * x;
 });
 
 console.log(newNumber);
 
 // Arrow Function way of doing this if there are 2 values:
-const newNumber = numbers.map( (x, y) => x * y);
+var newNumber = numbers.map( (x, y) => x * y);
 
 console.log(newNumber);
 
 // If the value is singular, you can remove the parenthesis around the accepted values as well:
-const newNumber = numbers.map(x => x * x;);
+var newNumber = numbers.map(x => x * x);
 
 ////Map -Create a new array by doing something with each item in an array.
-const newNumbers = numbers.map( x => x * 2);
+var newNumbers = numbers.map( x => x * 2);
 
 console.log(newNumbers);
 
 //////Filter - Create a new array by keeping the items that return true.
-const newNumbers = numbers.filter( num => num < 10);
+var newNumbers = numbers.filter( num => num < 10);
 
 console.log(newNumbers);
 
@@ -38,11 +38,11 @@ var newNumber = numbers.reduce((accumulator, currentNumber) => accumulator + cur
 console.log(newNumber);
 
 ////Find - find the first item that matches from an array.
-const newNumber = numbers.find(num => num > 10);
+var newNumber = numbers.find(num => num > 10);
 
 console.log(newNumber);
 
 ////FindIndex - find the index of the first item that matches.
-const newNumber = numbers.findIndex( num => num > 10);
+var newNumber = numbers.findIndex( num => num > 10);
 
 console.log(newNumber);
