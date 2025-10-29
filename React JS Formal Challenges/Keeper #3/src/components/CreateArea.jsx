301837e8ba761db2d5b4facc5ad597cc94c89daa
Gareque <grigeral@gmail.com>
2025-10-29 19:05:33 +0000

    Adding the Keeper 3 challenge

---
+++ React JS Formal Challenges/Keeper #3/src/components/CreateArea.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => ({
+      ...prevNote,
+      [name]: value,
+    }));
+  }
+
+  function submitNote(event) {
+    event.preventDefault(); //Prevents a page reload
+    props.onAdd(note); // Sends the note to the App
+    setNote({ title: "", content: "" }); // To clear the inputs
+  }
+
+  return (
+    <div>
+      <form>
+        <input
+          onChange={handleChange}
+          name="title"
+          placeholder="Title"
+          value={note.title}
+        />
+        <textarea
+          onChange={handleChange}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={note.content}
+        />
+        <button onClick={submitNote}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
